bdef82d10d38432141218e1f8d8fb1ff32899202
Eshin Ang <eshinang@hotmail.com>
2017-10-10 14:21:21 -0700

    Show answers

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -21,7 +21,7 @@ len <- length(all.numbers)
 ### Bonus ###
 
 # Create a vector `odd` that holds the odd numbers from 1 to 100
-odd <- (1, 100, 2)
+odd <- seq(1, 100, 2)
 
 # Using the `all` and `%%` operators, confirm that all of the numbers in your `odd` vector are odd
 test <- all(odd %% 2 == 1)
